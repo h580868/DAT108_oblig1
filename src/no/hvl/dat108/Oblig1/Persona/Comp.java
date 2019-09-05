19cb8c8ab4ca9cb86587ad5051a054a5dbf03cc3
Niklas Johansson <576966@hvl.no>
2019-09-05 13:54:34 +0200

    probably my most valuable contribution!

--- src/no/hvl/dat108/Oblig1/Persona/Comp.java
+++ src/no/hvl/dat108/Oblig1/Persona/Comp.java
@@ -3,6 +3,11 @@ package no.hvl.dat108.Oblig1.Persona;
 public class Comp {
 
     public static void main(String[] args) {
+
+        //sout + tab = System.out.println
+
+        // psvm + tab = public static .....
+
         
     }
 }
