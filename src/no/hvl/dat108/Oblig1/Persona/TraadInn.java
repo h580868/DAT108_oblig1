fc77041cea13680c7f5f177e780d62e72f8952c7
screenDesk <screenfilleddesk@gmail.com>
2019-09-05 14:49:03 +0200

    Added thread classes

--- src/no/hvl/dat108/Oblig1/Persona/TraadInn.java
+++ src/no/hvl/dat108/Oblig1/Persona/TraadInn.java
@@ -1,8 +1,6 @@
 package no.hvl.dat108.Oblig1.Persona;
 
 public class TraadInn implements Runnable{
-
-
     @Override
     public void run() {
 
--- src/no/hvl/dat108/Oblig1/Persona/TraadUt.java
+++ src/no/hvl/dat108/Oblig1/Persona/TraadUt.java
@@ -1,4 +1,8 @@
 package no.hvl.dat108.Oblig1.Persona;
 
-public class TraadUt {
+public class TraadUt implements Runnable{
+    @Override
+    public void run() {
+
+    }
 }
