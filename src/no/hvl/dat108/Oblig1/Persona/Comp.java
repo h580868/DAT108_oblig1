09e225fae22e4174175f315ae29d062b51df6133
screenDesk <screenfilleddesk@gmail.com>
2019-09-05 13:41:43 +0200

    Added main
    Blahb lah main added

---
+++ src/no/hvl/dat108/Oblig1/Persona/Comp.java
@@ -0,0 +1,8 @@
+package no.hvl.dat108.Oblig1.Persona;
+
+public class Comp {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/no/hvl/dat108/Oblig1/Persona/Person.java
+++ src/no/hvl/dat108/Oblig1/Persona/Person.java
@@ -3,5 +3,5 @@ package no.hvl.dat108.Oblig1.Persona;
 public class Person {
     String fName;
     String lName;
-    
+
 }
