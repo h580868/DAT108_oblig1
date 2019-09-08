7aa4efdeaf26ecdb016a990a65eeb255c7980644
Niklas Johansson <576966@hvl.no>
2019-09-08 23:44:53 +0200

    2a, Solved

--- src/no/hvl/dat108/Oblig1/Opp2/Main.java
+++ src/no/hvl/dat108/Oblig1/Opp2/Main.java
@@ -6,8 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
-        Collections.sort(listen);
-        Collections.sort(listen, (s1, s2) -> s1.length()-s2.length());
-        System.out.println(listen);
+       Collections.sort(listen,(s1,s2)-> Integer.parseInt(s1)-Integer.parseInt(s2));
+       System.out.println(listen);
     }
 }
