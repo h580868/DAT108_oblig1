d53173c427e831d8752d432b0387fa919497e2ef
screenDesk <screenfilleddesk@gmail.com>
2019-09-12 15:49:00 +0200

    Fixing more task 4

--- src/no/hvl/dat108/Oblig1/Opp4/Main.java
+++ src/no/hvl/dat108/Oblig1/Opp4/Main.java
@@ -8,23 +8,38 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    private List<String> bareEtternavn(List<Ansatt> liste) {
-
+    private static List<String> bareEtternavn(List<Ansatt> liste) {
         return liste.stream().map(a -> a.getEtternavn()).collect(Collectors.toList());
-
     }
 
     private static int antallKvinner(List<Ansatt> liste) {
         return (int)liste.stream().filter(a -> a.getKjonn() == Kjonn.KVINNE).count();
     }
 
+    private static double kvinneGjennomLonn(List<Ansatt> liste) {
+        List<Integer> kvinneLonnListe = liste.stream().filter(a -> a.getKjonn() == Kjonn.KVINNE).map(a -> a.getAarslonn()).collect(Collectors.toList());
+        double gjennLonn = kvinneLonnListe.stream().mapToDouble(a -> a.doubleValue()).sum() / kvinneLonnListe.size();
+        return gjennLonn;
+    }
+
+    private static void sjefLonnsOkning(List<Ansatt> liste) {
+        liste.stream()
+                .filter(a -> a.getStilling().toLowerCase().contains("sjef"))
+                .forEach(a -> a.setAarslonn((int)(a.getAarslonn() * 1.07)));
+    }
+
     public static void main(String[] args) {
 
         List<Ansatt> ansatte = new LinkedList<Ansatt>();
         Ansatt jim = new Ansatt("Jim", "Jimson", Kjonn.MANN, "Coffeemaker", 50);
+        Ansatt jane = new Ansatt("Jane", "Janesdaughter", Kjonn.KVINNE, "Sjef", 50000);
         ansatte.add(jim);
+        ansatte.add(jane);
 
-        System.out.println("" + antallKvinner(ansatte));
+        bareEtternavn(ansatte).forEach(a -> System.out.println(a));
+        System.out.println("Antall kvinner: " + antallKvinner(ansatte));
+        System.out.println("Kvinnelig gjennomsnittslonn: " + kvinneGjennomLonn(ansatte));
+        System.out.println("Ny lonn for sjef" + ansatte.stream().filter(a -> a.getStilling().toLowerCase().contains("sjef")).map(a -> a.getAarslonn()).forEach(a -> a.toString(););
 
     }
 }
