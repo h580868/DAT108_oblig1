a02ef31a537f7eefb54915927064c9ea6dc3eea8
screenfilleddesk@gmail.com <screenfilleddesk@gmail.com>
2019-09-17 20:55:54 +0200

    Opg3 Dummy method done

--- src/no/hvl/dat108/Oblig1/Opp3/Main.java
+++ src/no/hvl/dat108/Oblig1/Opp3/Main.java
@@ -2,15 +2,26 @@ package no.hvl.dat108.Oblig1.Opp3;
 
 import java.util.List;
 import java.util.function.Function;
+import static java.lang.System.out;
 
 public class Main {
 
-    void lonnsTillegg(Ansatt ansatt) {
-
+    void lonnsTillegg(List<Ansatt> liste) {
+        //hmm.. I'll just make one line for each thing
+        liste //+25 as a flat salary raise
+                .forEach(a -> a.setAarslonn(a.getAarslonn() + 25));
+        liste //+1% flat raise
+                .forEach(a -> a.setAarslonn((int)(a.getAarslonn() * 1.01)));
+        liste.stream() //+50 aarslonn for having low aarslonn
+                .filter(a -> a.getAarslonn() < 75000).forEach(a -> a.setAarslonn(a.getAarslonn() + 50));
+        liste.stream() //+5% salary for being a man
+                .filter(a -> a.getKjonn() == Kjonn.MANN).forEach(a -> a.setAarslonn((int)(a.getAarslonn() * 1.05)));
+        liste.stream() //+6% salary for being a woman
+                .filter(a -> a.getKjonn() == Kjonn.KVINNE).forEach(a -> a.setAarslonn(((int)(a.getAarslonn() * 1.06))));
     }
 
     void skrivUtAlle(List<Ansatt> ansatte){
-        ansatte.forEach(n -> System.out.print(n));
+        ansatte.forEach(out::print);
     }
 
     private static void lonnsOppgjor(List<Ansatt> ansatte, Function<Ansatt, Integer> someFunction) {
@@ -23,7 +34,7 @@ public class Main {
         Ansatt a1 = new Ansatt("Hassan", "Ali", Kjonn.MANN,"Student", 50000);
         Ansatt a2 = new Ansatt("Katarina", "Nederlid", Kjonn.KVINNE, "Student", 50001);
 
-        System.out.println(a1);
-        System.out.println(a2);
+        out.println(a1);
+        out.println(a2);
     }
 }
